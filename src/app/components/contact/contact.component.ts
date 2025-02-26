c7018ac3cf8e355d5d4e06cd145a35ee9bbe8ae7
Al Shariar <35493040+Shariarbup@users.noreply.github.com>
2025-02-26 20:14:28 +0600

    added emailjs form to my dynamic form

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import * as AOS from 'aos';
+import emailjs from 'emailjs-com';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -8,6 +10,13 @@ import * as AOS from 'aos';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit{
+  contactForm = new FormGroup({
+    user_name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    user_email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [Validators.required, Validators.minLength(10)])
+  });
+  isLoading = false;
+
   constructor(private router: Router) {}
 
   ngOnInit(): void {
@@ -20,6 +29,28 @@ export class ContactComponent implements OnInit{
         AOS.refresh(); // Reinitialize AOS on route change
       }
     });
+
+   
   }
+
+  sendEmail() {
+    if (this.contactForm.valid) {
+      this.isLoading = true;
+      emailjs.send('service_lnrq7a9', 'template_koubble', this.contactForm.value, 'TD2FakbB0iy1gZxAq')
+        .then(response => {
+          console.log('Email sent!', response);
+          alert('Your message has been sent!');
+          this.contactForm.reset();
+        })
+        .catch(error => {
+          console.error('Error sending email', error);
+          alert('Failed to send email.');
+        })
+        .finally(() => {
+          this.isLoading = false; // Hide loading state
+        });;
+    }
+  }
+
 
 }
